f12420a42bb03d851591707962ac9a9313f7e5de
alonbril <alonbril5@gmail.com>
2023-01-20 18:18:27 +0200

    side menu bar without action button wroking yet!

--- app/src/main/java/com/firstapp/avozer/Profile.java
+++ app/src/main/java/com/firstapp/avozer/Profile.java
@@ -19,8 +19,8 @@ public class Profile extends AppCompatActivity {
     Button logout;
     TextView name, mail;
     TabLayout tabLayout;
-    ViewPager2 viewPager2;
-    MyViewPagerAdapter myViewPagerAdapter;
+
+
 
 
     @Override
@@ -28,37 +28,11 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_profile);
 
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager2 = findViewById(R.id.view_pager);
-        myViewPagerAdapter = new MyViewPagerAdapter(this);
-        viewPager2.setAdapter(myViewPagerAdapter);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager2.setCurrentItem(tab.getPosition());
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
 
 
-            }
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
 
-            }
-        });
 
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                tabLayout.getTabAt(position).select();
-            }
-        });
 
 
 
