444d1bfb340db0830d4a1979871529edd34ffdd4
alonbril <alonbril5@gmail.com>
2023-02-03 12:49:39 +0200

    add recycler view , AdapterClass its not crashing but not working yet

--- app/src/main/java/com/firstapp/avozer/fragments/findRequestFormFragment.java
+++ app/src/main/java/com/firstapp/avozer/fragments/findRequestFormFragment.java
@@ -2,13 +2,26 @@ package com.firstapp.avozer.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firstapp.avozer.AdapterClass;
+import com.firstapp.avozer.DealsToShowFromDB;
 import com.firstapp.avozer.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +38,11 @@ public class findRequestFormFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    AdapterClass adapterClass;
+    ArrayList<DealsToShowFromDB>list;
+
 
     public findRequestFormFragment() {
         // Required empty public constructor
@@ -61,6 +79,101 @@ public class findRequestFormFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_request_form, container, false);
+        View view= inflater.inflate(R.layout.fragment_find_request_form, container, false);
+
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        dataInitialize();
+        ///////
+        recyclerView = view.findViewById(R.id.recyclerView);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("deals").child("dealID");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        list = new ArrayList<>();
+        adapterClass = new AdapterClass(getActivity(), list);
+        recyclerView.setAdapter(adapterClass);
+
+        ValueEventListener valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    DealsToShowFromDB dealsToShowFromDB = dataSnapshot.getValue(DealsToShowFromDB.class);
+                    list.add(dealsToShowFromDB);
+                }
+                adapterClass.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+
+
+    }
+
+//    private void dataInitialize() {
+//        recyclerView = getView().findViewById(R.id.findRequestFormFragment);
+//        databaseReference = FirebaseDatabase.getInstance().getReference().child("deals").child("dealID");
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+//
+//        list = new ArrayList<>();
+//        adapterClass = new AdapterClass(getActivity(), list);
+//        recyclerView.setAdapter(adapterClass);
+//
+//        ValueEventListener valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                    DealsToShowFromDB dealsToShowFromDB = dataSnapshot.getValue(DealsToShowFromDB.class);
+//                    list.add(dealsToShowFromDB);
+//                }
+//                adapterClass.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {}
+//        });
+//    }
+
+
+    //  private void dataInitialize() {
+    //    recyclerView =recyclerView.findViewById(R.id.findRequestFormFragment);
+      //  databaseReference= FirebaseDatabase.getInstance().getReference("Deals/type");
+        //recyclerView.setHasFixedSize(true);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+       // list = new ArrayList<>();
+       // adapterClass = new AdapterClass(getActivity(),list);
+        //recyclerView.setAdapter(adapterClass);
+
+
+     //   ValueEventListener valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+       //     @Override
+         //   public void onDataChange(@NonNull DataSnapshot snapshot) {
+           //     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+             //       DealsToShowFromDB dealsToShowFromDB = dataSnapshot.getValue(DealsToShowFromDB.class);
+               //     list.add(dealsToShowFromDB);
+
+
+               // }
+               // adapterClass.notifyDataSetChanged();
+
+            //}
+
+           // @Override
+            //public void onCancelled(@NonNull DatabaseError error) {}
+
+
+
+     //   }
+
+   // }
+
 }
