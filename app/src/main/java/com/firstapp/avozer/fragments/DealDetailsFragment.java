f4826940e8dcca520889c76d39f06752d82941eb
Vladimir <vovik.ds@gmail.com>
2023-02-07 16:28:03 +0200

    regenerate DealDetailsFragment

--- app/src/main/java/com/firstapp/avozer/fragments/DealDetailsFragment.java
+++ app/src/main/java/com/firstapp/avozer/fragments/DealDetailsFragment.java
@@ -17,8 +17,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import com.firstapp.avozer.AdapterClass;
-import com.firstapp.avozer.Deal;
 import com.firstapp.avozer.Person;
 import com.firstapp.avozer.R;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,8 +43,35 @@ public class DealDetailsFragment extends Fragment {
     private String mClientPhone;
     private String mClientEmail;
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
     public DealDetailsFragment() {
         // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DealDetailsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DealDetailsFragment newInstance(String param1, String param2) {
+        DealDetailsFragment fragment = new DealDetailsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
@@ -58,30 +84,12 @@ public class DealDetailsFragment extends Fragment {
             mCity = getArguments().getString(ARG4);
             mClientId = getArguments().getString(ARG5);
         }
+        getUserData();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        getUserData();
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().
-                child("users").child(mClientId);
-
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Person client = snapshot.getValue(Person.class);
-                mClientFirstName = client.firstName;
-                mClientLastName = client.lastName;
-                mClientPhone = client.phone;
-                mClientEmail = client.email;
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_deal_details, container, false);
@@ -115,9 +123,23 @@ public class DealDetailsFragment extends Fragment {
         return view;
     }
 
-    public void getUserData(){
+    public void getUserData() {
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().
+                child("users").child(mClientId);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Person client = snapshot.getValue(Person.class);
+                mClientFirstName = client.firstName;
+                mClientLastName = client.lastName;
+                mClientPhone = client.phone;
+                mClientEmail = client.email;
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
-
-
