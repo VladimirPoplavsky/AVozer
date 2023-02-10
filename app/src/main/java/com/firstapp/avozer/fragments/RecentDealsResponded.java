98a3898ffd836bde99fdc2ce6e84f7c01fd1d6a2
vovik <vovik.ds@gmail.com>
2023-02-10 12:44:21 +0200

    Add conditions to show deals to: - UpcomingDealsResponded - RecentDealsResponded

--- app/src/main/java/com/firstapp/avozer/fragments/RecentDealsResponded.java
+++ app/src/main/java/com/firstapp/avozer/fragments/RecentDealsResponded.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firstapp.avozer.AdapterClass;
+import com.firstapp.avozer.Deal;
 import com.firstapp.avozer.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +31,8 @@ public class RecentDealsResponded extends Fragment {
     RecyclerView recyclerView;
 
     AdapterClass adapterClass;
+
+    private ArrayList<Deal> dealsList = new ArrayList<Deal>();
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -71,6 +82,8 @@ public class RecentDealsResponded extends Fragment {
         View view = inflater.inflate(R.layout.fragment_recent_deals_responded,
                 container, false);
 
+        getDeals();
+
         super.onViewCreated(view, savedInstanceState);
 //        dataInitialize();
         ///////
@@ -80,14 +93,55 @@ public class RecentDealsResponded extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
 //        list = new ArrayList<Deal>();
-        adapterClass = new AdapterClass(getActivity(), ProfileFragment.list);
+        adapterClass = new AdapterClass(getActivity(), dealsList);
         recyclerView.setAdapter(adapterClass);
 
 
         adapterClass.notifyDataSetChanged();
 
-
-
         return view;
     }
+
+    private void getDeals() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        assert user != null;
+        String currentUid = user.getUid();
+        String helperUid = "";
+        String dealDate = "";
+        SimpleDateFormat sdf = null;
+        Date d = null;
+        long currentTimeMillis = System.currentTimeMillis();
+        long dealTimeMillis = 0;
+        int listSize = ProfileFragment.list.size();
+        boolean helperIsFound;
+
+        for (int i = 0; i < listSize - 1; i++) {
+
+            helperIsFound = ProfileFragment.list.get(i).helperIsFound;
+
+            if(helperIsFound) {
+                helperUid = ProfileFragment.list.get(i).helperUid;
+                dealDate = ProfileFragment.list.get(i).whenNeedHelp;
+                sdf = new SimpleDateFormat("d/M/y H:m");
+                try {
+                    d = sdf.parse(dealDate);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+                assert d != null;
+                dealTimeMillis = d.getTime();
+
+                if (currentUid.equals(helperUid)
+                        && currentTimeMillis >= dealTimeMillis) {
+                    dealsList.add(ProfileFragment.list.get(i));
+                }
+            }
+        }
+
+        // dummy element to prevent exception
+        if(dealsList.size() == 0){
+            Toast.makeText(getActivity(), "There are any open requests at the moment",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
