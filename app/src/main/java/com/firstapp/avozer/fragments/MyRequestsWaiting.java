2c38878487e73b56c63cede27d24ce65cf8a9198
alonbril <alonbril5@gmail.com>
2023-02-10 13:52:25 +0200

    edit for lops

--- app/src/main/java/com/firstapp/avozer/fragments/MyRequestsWaiting.java
+++ app/src/main/java/com/firstapp/avozer/fragments/MyRequestsWaiting.java
@@ -106,7 +106,7 @@ public class MyRequestsWaiting extends Fragment {
         int listSize = ProfileFragment.list.size();
         boolean helperIsFound;
 
-        for (int i = 0; i < listSize - 1; i++) {
+        for (int i = 0; i < listSize; i++) {
             helperIsFound = ProfileFragment.list.get(i).helperIsFound;
             dealClientUid = ProfileFragment.list.get(i).clientUid;
 
